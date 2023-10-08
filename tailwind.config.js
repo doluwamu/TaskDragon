9928d2cd22047c2e9af00e82db6765689e84450b
doluwamu <dboyadeitan@gmail.com>
2023-10-08 18:07:08 +0100

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    screens: {
+      xsm: "479.99px",
+      sm: "480px",
+      md: "768px",
+      lg: "1000px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        "brown-500": "hsla(25, 100%, 7%, 1)",
+        "black-800": "#0008",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  content: [],
+};
