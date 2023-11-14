5b2ba71dc05e1a1ffe349a810df1bc16d68db488
doluwamu <dboyadeitan@gmail.com>
2023-11-14 15:36:18 +0100

    feat: implemented user profile edit feature

---
+++ controllers/user/updateUserProfile.js
@@ -0,0 +1,35 @@
+import User from "../../models/User.js";
+import asyncHandler from "express-async-handler";
+
+// @desc Edit a user's profile
+// @route POST /api/v1/users/me/edit
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const { user } = req;
+  const { username, email } = req.body;
+
+  // if (!userId) return res.status(400).json({ message: "No user id found" });
+
+  // if (user._id !== userId)
+  //   return res.status(400).json({ message: "This is not your id" });
+
+  const foundUser = await User.findById(user.id);
+
+  if (foundUser.email === email)
+    return res.status(400).json({ message: "User with this email exists" });
+
+  if (!foundUser)
+    return res.status(404).json({ message: "User does not exist" });
+
+  foundUser.username = username || foundUser.username;
+  foundUser.email = email || foundUser.email;
+
+  const updatedProfile = await foundUser.save();
+
+  return res.json({
+    message: "Profile successfullt updated",
+    userInfo: updatedProfile,
+  });
+});
+
+export default updateUserProfile;
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ import express from "express";
 import getUser from "../controllers/user/getUser.js";
 import getUsers from "../controllers/user/getUsers.js";
 import setUserSecrets from "../controllers/user/setUserSecrets.js";
+import updateUserProfile from "../controllers/user/updateUserProfile.js";
 import verifyUser from "../controllers/user/verifyUser.js";
 import {
   isAdmin,
@@ -13,7 +14,8 @@ const router = express.Router();
 
 // User
 router.post("/secret/:userId", userLoggedIn, setUserSecrets);
-router.get("/me", userLoggedIn, getUser);
+router.route("/me", userLoggedIn, userIsVerified, getUser);
+router.put("/me/edit", userLoggedIn, userIsVerified, updateUserProfile);
 router.post("/verify/:userId", userLoggedIn, verifyUser);
 
 // Admin only
