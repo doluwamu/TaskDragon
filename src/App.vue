4382b2808400c329c4af7d26e661064a343437e6
doluwamu <dboyadeitan@gmail.com>
2023-11-20 20:24:26 +0100

    fix: edited style for task edit modal

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import Navbar from './components/app/Navbar.vue'
-// import jwt from 'jsonwebtoken'
 </script>
 
 <template>
-  <!-- <div>
-    <Navbar /> -->
   <RouterView />
-  <!-- </div> -->
 </template>
--- src/components/tasks/EditTaskModal.vue
+++ src/components/tasks/EditTaskModal.vue
@@ -26,111 +26,115 @@
       <!-- <p class="text-red-600 text-center" v-if="errMsg.length > 0">{{ errMsg }}</p>
       <p class="text-green-700 text-center" v-if="successMsg.length > 0">{{ successMsg }}</p> -->
 
-      <form @submit.prevent="editTask(task?._id)">
-        <!-- Title -->
-        <div class="flex flex-col py-5">
-          <label
-            >Title:
-            <span :class="`${title?.length <= 200 ? 'text-green-400' : 'text-red-600'}`">
-              ({{ title?.length }}/200)</span
-            ></label
-          >
-          <input type="text" v-model="title" class="text-3xl w-full text-white" />
-        </div>
-
-        <!-- Description -->
-        <div class="flex flex-col py-5">
-          <label>Description:</label>
-          <textarea
-            name="description"
-            v-model="description"
-            class="p-2 max-w-4/5 resize-none w-full my-4"
-            cols="30"
-            rows="6"
-          ></textarea>
-        </div>
-
-        <div class="details p-2 flex flex-col gap-4">
-          <div class="flex flex-col justify-evenly gap-5 sm:gap-1 md:flex-row">
-            <div class="flex flex-wrap justify-center gap-5 flex-row sm:justify-center">
-              <!-- Priority -->
-              <div class="flex flex-col gap-1 text-lg">
-                <p>Priority:</p>
-                <select name="priority" v-model="priority">
-                  <!-- <option class="text-gray-600" value="">{{ priority }}</option> -->
-                  <option value="normal" class="text-gray-800">Normal</option>
-                  <option value="important" class="text-gray-800">Important</option>
-                  <option value="very-important" class="text-gray-800">Very important</option>
-                </select>
-              </div>
-
-              <!-- Status -->
-              <div class="flex flex-col gap-1 text-lg">
-                <p class="text-white">Status:</p>
-                <select name="priority" v-model="status">
-                  <!-- <option class="text-gray-600" value="">{{ priority }}</option> -->
-                  <option value="undone" class="text-red-600">Undone</option>
-                  <option value="doing" class="text-orange-400">Doing</option>
-                  <option value="done" class="text-green-500">Done</option>
-                </select>
-                <button
-                  v-if="taskStore.loaders.updateStatus"
-                  class="button p-2 text-sm bg-green-400"
-                >
-                  Loading...
-                </button>
-                <button
-                  v-else
-                  type="button"
-                  @click="editTaskStatus(task?._id)"
-                  class="button p-2 text-sm bg-green-600"
-                >
-                  Update status
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div class="foot flex justify-center gap-5 sm:justify-center">
-            <!-- Start time -->
-            <div class="flex flex-col text-xs gap-1 sm:flex-row">
-              <p>Started on:</p>
-              <p>{{ !task.startTime ? 'null' : moment(task?.startTime).format('MMM Do, YYYY') }}</p>
-            </div>
-
-            <!-- End time -->
-            <div class="flex flex-col text-xs gap-1 sm:flex-row">
-              <p>Ended on:</p>
-              <p>{{ !task.endTime ? 'null' : moment(task?.endTime).format('MMM Do, YYYY') }}</p>
-            </div>
-          </div>
-
-          <div class="flex justify-center gap-5 pt-5">
-            <!-- Start time -->
-            <div v-if="task?.createdAt" class="flex text-xs gap-1">
-              <p>Created on:</p>
-              <p>{{ moment(task?.createdAt).format('MMM Do, YYYY') }}</p>
-            </div>
-          </div>
-
-          <div class="p-2 flex items-center justify-center">
-            <button
-              v-if="taskStore.loaders.updateTask"
-              type="button"
-              class="button bg-gray-600 text-white w-[150px] px-3 py-2"
+      <div class="flex flex-col justify-evenly md:flex-row">
+        <form @submit.prevent="editTask(task?._id)" class="md:w-3/4">
+          <!-- Title -->
+          <div class="flex flex-col py-5">
+            <label
+              >Title:
+              <span :class="`${title?.length <= 200 ? 'text-green-400' : 'text-red-600'}`">
+                ({{ title?.length }}/200)</span
+              ></label
             >
-              Loading...
-            </button>
-            <button
-              v-else
-              type="submit"
-              class="button bg-black text-white text-center w-[150px] px-3 py-2"
-            >
-              Update
-            </button>
+            <input type="text" v-model="title" class="text-3xl w-full text-white" />
+          </div>
+
+          <!-- Description -->
+          <div class="flex flex-col py-5">
+            <label>Description:</label>
+            <textarea
+              name="description"
+              v-model="description"
+              class="p-2 max-w-4/5 resize-none w-full my-4"
+              cols="30"
+              rows="6"
+            ></textarea>
+          </div>
+
+          <div class="details p-2 flex flex-col gap-4">
+            <div class="flex flex-col justify-evenly gap-5 sm:gap-1 md:flex-row">
+              <div class="flex flex-wrap justify-center gap-5 flex-row sm:justify-center">
+                <!-- Priority -->
+                <div class="flex flex-col gap-1 text-lg">
+                  <p>Priority:</p>
+                  <select name="priority" v-model="priority">
+                    <!-- <option class="text-gray-600" value="">{{ priority }}</option> -->
+                    <option value="normal" class="text-gray-800">Normal</option>
+                    <option value="important" class="text-gray-800">Important</option>
+                    <option value="very-important" class="text-gray-800">Very important</option>
+                  </select>
+                </div>
+
+                <!-- Status -->
+                <div class="flex flex-col gap-1 text-lg">
+                  <p class="text-white">Status:</p>
+                  <select name="priority" v-model="status">
+                    <!-- <option class="text-gray-600" value="">{{ priority }}</option> -->
+                    <option value="undone" class="text-red-600">Undone</option>
+                    <option value="doing" class="text-orange-400">Doing</option>
+                    <option value="done" class="text-green-500">Done</option>
+                  </select>
+                  <button
+                    v-if="taskStore.loaders.updateStatus"
+                    class="button p-2 text-sm bg-green-400"
+                  >
+                    Loading...
+                  </button>
+                  <button
+                    v-else
+                    type="button"
+                    @click="editTaskStatus(task?._id)"
+                    class="button p-2 text-sm bg-green-600"
+                  >
+                    Update status
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div class="p-2 flex items-center justify-center">
+              <button
+                v-if="taskStore.loaders.updateTask"
+                type="button"
+                class="button bg-gray-600 text-white w-[150px] px-3 py-2"
+              >
+                Loading...
+              </button>
+              <button
+                v-else
+                type="submit"
+                class="button bg-black text-white text-center w-[150px] px-3 py-2"
+              >
+                Update
+              </button>
+            </div>
+          </div>
+        </form>
+
+        <!-- timestamps -->
+        <div
+          class="flex gap-5 justify-center flex-wrap md:flex-col md:justify-start md:mt-5 md:border-l md:border-[#fff2] md:p-5 md:w-1/5"
+        >
+          <!-- Start time -->
+          <div class="flex flex-col text-xs gap-1 sm:flex-row md:flex-col">
+            <p>Started on:</p>
+            <p>{{ !task.startTime ? 'null' : moment(task?.startTime).format('MMM Do, YYYY') }}</p>
+          </div>
+
+          <!-- End time -->
+          <div class="flex flex-col text-xs gap-1 sm:flex-row md:flex-col">
+            <p>Completed on:</p>
+            <p>{{ !task.endTime ? 'null' : moment(task?.endTime).format('MMM Do, YYYY') }}</p>
+          </div>
+
+          <!-- Created time -->
+          <div class="flex flex-col text-xs gap-1 sm:flex-row md:flex-col">
+            <p>Created on:</p>
+            <p v-if="task?.createdAt">{{ moment(task?.createdAt).format('MMM Do, YYYY') }}</p>
+            <p v-else>null</p>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   </div>
 </template>
