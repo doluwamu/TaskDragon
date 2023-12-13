ec15061d73fa4ee900776f9f52fbb31f017b588a
Doluwamu <dboyadeitan@gmail.com>
2023-12-13 19:47:33 +0100

    fix: updating ui for Event details

--- src/views/EventDetailsView.vue
+++ src/views/EventDetailsView.vue
@@ -1,14 +1,45 @@
 <template>
-  <div class="text-white"></div>
+  <div class="text-white">
+    <Navbar />
+
+    <div
+      class="flex justify-center items-center max-w-[1440px] min-h-[150px] bg-blue-900 py-6 px-3"
+    >
+      <!-- <h1 class="text-2xl">Event name:</h1> -->
+      <p class="text-5xl">{{ eventData?.name }}</p>
+    </div>
+
+    <div class="flex flex-col gap-5 justify-center items-center py-8 px-2 md:px-4">
+      <!-- <h1 class="text-5xl px-2 py-10 text-center">Event Details</h1> -->
+      <div class="flex flex-col">
+        <!-- <h1 class="text-2xl">Event Description:</h1> -->
+        <p class="text-xl font-light">{{ eventData?.description }}</p>
+      </div>
+
+      <div class="flex flex-col">
+        <!-- <h1 class="text-2xl">Event Status:</h1> -->
+        <p class="text-xl">{{ eventData?.status }}</p>
+      </div>
+
+      <div class="flex flex-col">
+        <!-- <h1 class="text-2xl">Event name:</h1> -->
+        <p class="text-xl">{{ eventData?.name }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { useEventStore } from '../stores/events'
+import Navbar from '../components/app/Navbar.vue'
 
 const eventStore = useEventStore()
 
 export default {
   name: 'EventDetails',
+  components: {
+    Navbar
+  },
   data() {
     return {
       eventData: {}
